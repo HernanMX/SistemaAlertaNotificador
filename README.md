dab2ce60f81d03013603b3ddb14664c585e55231
HernanMX <niu1982@hotmail.com>
2018-09-27 01:09:00 -0500

    Version 2

--- README.md
+++ README.md
@@ -1,27 +1,42 @@
-PRACTICA #2 25 de Septiembre del 2018
+
+Practica #2 "Sistema Notificador de Individuos"
+===============================================
+
+Software
+--------
++Arduino IDE
+
++Ubuntu 16.04
 
 
+Librerias
+---------
++LowPower.h
 
-"Sistema Notificador de Llegada de Individuos"
 
-
-
+Descripción del proyecto
+------------------------
 El siguiente programa es un sistema de alerta para una puerta la cual detecta individuos.
 Cuando se abre la puerta debido a que se acerca un inidividuo se emite una alarma 
 que consiste de una Luz led y sonido.
-En el sistema se debe reducir el consumo de energia del arduino al minimo 
-mientras la alarma no este sonando.
-Una vez cerrada la puerta la alarma deja de sonar y emitir luz y el consumo de enrgia 
-alto.
+En el sistema se debe reducir el consumo de energia del arduino al minimo mientras 
+la alarma no este sonando.
+Una vez cerrada la puerta la alarma deja de sonar y emitir luz y el consumo de enrgia debe
+volver a ser el minimo.
 
 
+MATERIAL:
+---------
++1 Arduino uno
 
-MATERIAL
-*1 Arduino uno
-*1 LED
-*1 Red Switch
-*1 Resistencia de 220 Ohms
-*1 Buzzer
++1 LED
+
++1 Red Switch
+
++1 Resistencia de 220 Ohms
+
++1 Buzzer
+
 *1 Protoboard
 
 ![Imagen](https://github.com/HernanMX/SistemaAlertaNotificador/blob/master/sistema2.jpeg)
--- sistemaAlerta.ino
+++ sistemaAlerta.ino
@@ -1,39 +1,82 @@
-
+//Incluimos la libreria LowPower
+#include "LowPower.h"
 //Asignacion del reed switch
-#define reed 2
+#define puerta 2
 //Asignacion del led de alarma
-#define led  13
+#define luz  13
 //Asignacion del la buzzer para sonido alarma
-#define buzzer 9
-
-int duracion = 350; //Duración del sonido
-int fMin = 3000; //Frecuencia más baja que queremos emitir
-int fMax = 5000; //Frecuencia más alta que queremos emitir
-int i = 0;
+#define alarma 9
+//Declaracion de la varible que contendra la lectura de la puerta
 int valorSwitch;
+//Declaramos los parametros para emitir el sonido
+int length = 26;
+char notes[] = "eeeeeeegcde fffffeeeeddedg";
+int beats[] = { 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2};
+int tempo = 150;
 
 void setup() {
-  pinMode(reed, INPUT_PULLUP);
-  pinMode(led, OUTPUT);
-  pinMode (buzzer, OUTPUT);
+  pinMode(puerta, INPUT_PULLUP);
+  pinMode(luz, OUTPUT);
+  pinMode (alarma, OUTPUT);
+  //Se asigna una interrupcion 0 que va ligada a la puerta
+  attachInterrupt( 0, activador, FALLING);
+  //Se pone a dormir al arduino hasta que se presente una interrupcion
+  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
 }
 
 void loop() {
-  //Asignamos el valor del reed switch a la variable correspondiente
-  valorSwitch = digitalRead(reed);
+  //Si se detecta una interrupción en el pin 2 se enciende la
+  //la alarma mientras la puerta este abierta
+  detachInterrupt(0);
+    while (valorSwitch == LOW){
+    sonido();
+    valorSwitch = digitalRead(puerta);
+    }   
+  //Se asigna una interrupcion 0 que va ligada a la puerta
+  attachInterrupt( 0, activador, FALLING);
+  //Se pone a dormir al arduino hasta que se presente una interrupcion
+  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);  
+}
 
-  //Si el sensor es activado sonara la alarma así como se encedera el led
-  if (valorSwitch == LOW) {
-    digitalWrite(led, HIGH);
-  //Se reproduce el tono en el buzzer
-    for (i = fMin; i <= fMax; i++)
-      tone(buzzer, i, duracion);
-    for (i = fMax; i >= fMin; i--)
-      tone(buzzer, i, duracion);
-           
-  } else {
-    //Apagamos el led
-    digitalWrite(led, LOW);
+void activador(){
+  //Leemos el valor del reed switch
+  valorSwitch = digitalRead(puerta);
+}
+
+void sonido(){ 
+  //Metodo para hacer sonar la bocina con una melodia
+  for (int i = 0; i < length; i++) {
+    if (notes[i] == ' ') {
+      delay(beats[i] * tempo);
+    } else {
+      playNote(notes[i], beats[i] * tempo);
+    }
+    //Pausa entre las notas
+    delay(tempo / 2); 
   }
-  delay(1000);
+}
+
+void playTone(int tone, int duration) {
+  valorSwitch = digitalRead(puerta);
+  if( valorSwitch == LOW){
+    for (long i = 0; i < duration * 1000L; i += tone * 2) {
+      digitalWrite(luz, HIGH);
+      digitalWrite(alarma, HIGH);
+      delayMicroseconds(tone);
+      digitalWrite(luz, LOW);
+      digitalWrite(alarma, LOW);
+      delayMicroseconds(tone);    
+    }
+  }
+}
+
+void playNote(char note, int duration) {
+  char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
+  int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
+ 
+  for (int i = 0; i < 8; i++) {
+    if (names[i] == note) {
+      playTone(tones[i], duration);
+    }
+  }
 }
